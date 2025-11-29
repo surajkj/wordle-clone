d6ba59ed22d5c48d4f65991cc93b25733d4edfde
suraj <srj121.kj@gmail.com>
2025-11-29 20:23:14 +0900

    Fix build issue 12

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: process.env.GITHUB_PAGES ? '/wordle-clone/' : '/',
   plugins: [
     react(),
     tailwindcss(),
@@ -17,8 +18,8 @@ export default defineConfig({
         theme_color: '#3B82F6',
         background_color: '#ffffff',
         display: 'standalone',
-        start_url: '/wordle-clone/',
-        scope: '/',
+        start_url: process.env.GITHUB_PAGES ? '/wordle-clone/' : '/',
+        scope: process.env.GITHUB_PAGES ? '/wordle-clone/' : '/',
         icons: [
           {
             src: 'pwa-192x192.png',
@@ -41,30 +42,6 @@ export default defineConfig({
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
         navigateFallback: '/index.html',
-        navigateFallbackAllowlist: [/^(?!\/__).*/],
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'google-fonts-cache',
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 365 // 365 days
-              },
-              cacheableResponse: {
-                statuses: [0, 200]
-              }
-            }
-          },
-          {
-            urlPattern: /\.(?:js|css|html)$/,
-            handler: 'StaleWhileRevalidate',
-            options: {
-              cacheName: 'static-resources',
-            }
-          }
-        ]
       }
     })
   ],
